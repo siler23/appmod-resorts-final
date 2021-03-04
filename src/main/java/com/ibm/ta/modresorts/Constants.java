db62dc5a9b52d79c54539981f632df4075bb85d6
Garrett Woodworth <glwoodworth@ucdavis.edu>
2021-03-04 03:23:53 +0000

    Testing with dogs

--- src/main/java/com/ibm/ta/modresorts/Constants.java
+++ src/main/java/com/ibm/ta/modresorts/Constants.java
@@ -18,7 +18,7 @@ package com.ibm.ta.modresorts;
 class Constants {
 
 	public static final String BARCELONA = "Barcelona";
-	public static final String CORK = "Corgie";
+	public static final String CORK = "Corgies";
 	public static final String MIAMI = "Miami";
 	public static final String SAN_FRANCISCO = "San_Francisco";
 	public final static String PARIS = "Paris";
